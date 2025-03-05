bea822b90f52c98d2d263b1f9a3f2e8ca948c20a
kenwu <kenqiwu@gmail.com>
2025-03-04 18:35:05 -0600

    disable swiping back from left edge

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout() {
               onPress={() => router.navigate('/')}
             />
           ),
+          gestureEnabled: false,
         }}
       />
     </PaperProvider>
