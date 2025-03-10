543cfda1e5939708c647d34ca175fcaeae784cc3
kenwu <kenqiwu@gmail.com>
2025-03-10 10:58:08 -0500

    add styling mocks and constants

--- src/constants/stylingConstants.ts
+++ src/constants/stylingConstants.ts
@@ -1,16 +1,32 @@
 import {StyleSheet} from "react-native";
+import {
+  Montserrat_500Medium,
+  Montserrat_700Bold,
+  Montserrat_800ExtraBold,
+  Montserrat_900Black,
+  useFonts
+} from "@expo-google-fonts/montserrat";
 
 // come in and change the entire look of the app without knowing how to code
 // this contains all the constants necessary for a graphics designer to
 
 
 // Text and Font
+export default () => {
+  let [fontsLoaded] = useFonts({
+    Montserrat_500Medium,
+    Montserrat_700Bold,
+    Montserrat_800ExtraBold,
+    Montserrat_900Black,
+  });
+};
 
 // Colors
-
-export const YELLOW = "#fced60"
-export const PURPLE = "#b467e6"
-export const NAVY = "#14265f"
+export const YELLOW = "#ffec3b"
+export const ORANGE = "#f39a3e"
+export const PURPLE = "#c062ed"
+export const BLUE = "#2355e2"
+export const NAVY = "#0d2763"
 
 // Components
 
@@ -33,6 +49,22 @@ export const styleSheet = StyleSheet.create({
   MENU_BACKGROUND: {
     backgroundColor: NAVY,
   }
+})
+
+// Stylesheets: Fonts
+export const FONT = StyleSheet.create({
+  MONTSERRAT_500MEDIUM: {
+    fontFamily: 'Montserrat_500Medium',
+  },
+  MONTSERRAT_700BOLD: {
+    fontFamily: 'Montserrat_700Bold',
+  },
+  MONTSERRAT_800EXTRABOLD: {  // titles and game score
+    fontFamily: 'Montserrat_800ExtraBold',
+  },
+  MONTSERRAT_900BLACK: {  // game word
+    fontFamily: 'Montserrat_900Black',
+  },
 })
 
 // Stylesheets: Home Menu - index.tsx
